00fd608dc5da70dc8019d23b4ecb4a328223b3de
Simon Gellis <simongellis@gmail.com>
2021-12-07 21:43:03 -0500

    Double the size of the stacks

--- src/rust-bootstrapper/compiler.rs
+++ src/rust-bootstrapper/compiler.rs
@@ -69,8 +69,8 @@ pub struct Compiler {
 }
 
 const DICTIONARY_BASE: i32 = 0x1000;
-const PARAM_STACK_BASE: i32 = 0xf000;
-const RETURN_STACK_BASE: i32 = 0xf080;
+const PARAM_STACK_BASE: i32 = 0xef00;
+const RETURN_STACK_BASE: i32 = 0xf000;
 const HEAP_BASE: i32 = 0xf100;
 
 const DICTIONARY_CAPACITY: i32 = PARAM_STACK_BASE - DICTIONARY_BASE;
